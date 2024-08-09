3841bc4108f7b3f7040f3e698cee076cc8a0d93b
hedon954 <171725713@qq.com>
2024-08-10 00:07:10 +0800

    fix(timer): fix delay timer mock of clear room timeout to pass test

--- internal/config/mock/delay_timer_mock.go
+++ internal/config/mock/delay_timer_mock.go
@@ -6,17 +6,19 @@ import (
 )
 
 const (
-	InviteTimeoutMs   = 300000
-	MatchTimeoutMs    = 60000
-	WaitAttrTimeoutMs = 1
+	InviteTimeoutMs    = 300 * 1000
+	MatchTimeoutMs     = 60 * 1000
+	WaitAttrTimeoutMs  = 1
+	ClearRoomTimeoutMs = 30 * 60 * 1000
 )
 
 type DelayTimerMock struct{}
 
 func (d *DelayTimerMock) GetConfig(_ constant.GameMode) config.DelayTimerConfig {
 	return config.DelayTimerConfig{
-		InviteTimeoutMs:   InviteTimeoutMs,
-		MatchTimeoutMs:    MatchTimeoutMs,
-		WaitAttrTimeoutMs: WaitAttrTimeoutMs,
+		InviteTimeoutMs:    InviteTimeoutMs,
+		MatchTimeoutMs:     MatchTimeoutMs,
+		WaitAttrTimeoutMs:  WaitAttrTimeoutMs,
+		ClearRoomTimeoutMs: ClearRoomTimeoutMs,
 	}
 }
